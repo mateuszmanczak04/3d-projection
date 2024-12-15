db810be92a99c504eadc79e76d9449eb381e731b
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-12-15 14:52:45 +0100

    Create rendering engine

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const translation = [400, 300];
+const scale = 2;
+
 const vertices = [
 	[0, 0, 0],
 	[100, 0, 0],
@@ -14,8 +17,50 @@ const edges = [
 	[0, 0, 1, 0, 0, 1, 0, 0],
 	[0, 0, 0, 1, 0, 0, 1, 0],
 	[0, 0, 0, 0, 0, 0, 0, 1],
-	[0, 0, 0, 0, 0, 1, 0, 0],
+	[0, 0, 0, 0, 0, 1, 0, 1],
 	[0, 0, 0, 0, 0, 0, 1, 0],
 	[0, 0, 0, 0, 0, 0, 0, 1],
 	[0, 0, 0, 0, 0, 0, 0, 0],
 ];
+
+const camera = [50, 50, -100];
+
+const screenCenter = [50, 50, -50];
+
+const projectedVertices = vertices.map(([x, y, z]) => {
+	const fromCameraToScreen = Math.abs(screenCenter[2] - camera[2]);
+	const fromScreenToPoint = Math.abs(z - screenCenter[2]);
+	const ratio = fromCameraToScreen / fromScreenToPoint;
+
+	const xDistance = camera[0] - x;
+	const projectedX = camera[0] - xDistance * ratio;
+
+	const yDistance = camera[1] - y;
+	const projectedY = camera[1] + yDistance * ratio;
+
+	return [projectedX, projectedY];
+});
+
+const canvas = document.getElementById('app-canvas');
+const ctx = canvas.getContext('2d');
+
+projectedVertices.forEach(([x, y]) => {
+	ctx.beginPath();
+	ctx.arc(x * scale + translation[0], y * scale + translation[1], 4, 0, Math.PI * 2);
+	ctx.fill();
+});
+
+edges.forEach((edge, i) => {
+	edge.forEach((connection, j) => {
+		if (connection === 1) {
+			const startPoint = projectedVertices[i];
+			const endPoint = projectedVertices[j];
+			ctx.moveTo(
+				startPoint[0] * scale + translation[0],
+				startPoint[1] * scale + translation[1],
+			);
+			ctx.lineTo(endPoint[0] * scale + translation[0], endPoint[1] * scale + translation[1]);
+			ctx.stroke();
+		}
+	});
+});
